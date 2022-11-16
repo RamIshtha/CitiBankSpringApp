80d1764eef56c6a1a9212f36f3f9e404846fb398
jyoti <TL@gmail.com>
2022-11-16 23:42:14 +0530

    dev--otp commited

--- SpringHibernateProj/src/com/rt/test/Test.java
+++ SpringHibernateProj/src/com/rt/test/Test.java
@@ -8,7 +8,11 @@ public class Test {
 		System.out.println("Hi abc.."); 
 		System.out.println("Hi user..");
 		System.out.println("Hi ignored..");
-
+		m1();
+	}
+	
+	private static void m1() {
+		System.out.println("m1");
 	}
 
 }
